74f6129dcff2ef7b62456c46413d353eee0f2d1c
John Carroll <jcarroll@kindsnacks.com>
2023-03-21 16:25:14 -0400

    update Jenkinsfile and runtime.sh

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,9 +3,12 @@ pipeline {
   stages {
     stage('Build') {
       steps {
-        sh './runtime.sh'
+		sh 'cd /home/ubuntu'
+		sh 'rm -rf easy-school/'
+		sh 'git clone https://github.com/teammira2023/easy-school.git'
+		sh 'chmod +x easy-school/runtime.sh'
+        sh 'easy-school/runtime.sh'
       }
     }
 
   }
-}
--- runtime.sh
+++ runtime.sh
@@ -3,9 +3,7 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
-rm -rf easy-school
-git clone https://github.com/teammira2023/easy-school.git
-cd easy-school
+cd /home/ubuntu/easy-school
 docker-compose build
 docker-compose run web python manage.py createsuperuser
 docker-compose up

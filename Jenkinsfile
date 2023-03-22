d744d6ae40a72dca492c7a72c68299281e5529b1
John Carroll <jcarroll@kindsnacks.com>
2023-03-21 22:21:51 -0400

    remove calls to /home/ubuntu

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,8 +3,6 @@ pipeline {
   stages {
     stage('Build') {
       steps {
-		sh 'cd /home/ubuntu'
-		sh 'rm -rf easy-school/'
 		sh 'git clone https://github.com/teammira2023/easy-school.git'
 		sh 'chmod +x easy-school/runtime.sh'
         sh 'easy-school/runtime.sh'
--- runtime.sh
+++ runtime.sh
@@ -3,7 +3,7 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
-cd /home/ubuntu/easy-school
-docker-compose build
-docker-compose run web python manage.py createsuperuser
-docker-compose up
+#cd /home/ubuntu/easy-school
+#docker-compose build
+#docker-compose run web python manage.py createsuperuser
+#docker-compose up

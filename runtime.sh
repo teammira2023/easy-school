61bbc1be670832d80ec8ae4c9f7e6a5e61b984bb
John Carroll <jcarroll@kindsnacks.com>
2023-03-21 22:54:47 -0400

    try runtime

--- runtime.sh
+++ runtime.sh
@@ -1,9 +1,9 @@
 
 #!/bin/sh
-docker stop \$(docker ps -a -q)
-docker rm \$(docker ps -a -q)
-docker rmi \$(docker images -a -q)
-#cd /home/ubuntu/easy-school
-#docker-compose build
-#docker-compose run web python manage.py createsuperuser
-#docker-compose up
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+#docker rmi $(docker images -a -q)
+cd easy-school
+docker-compose build
+docker-compose run web python manage.py createsuperuser
+docker-compose up

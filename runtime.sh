e1beb74718abd5641d529ec9f0021b2f8b834d06
John Carroll <jcarroll@kindsnacks.com>
2023-04-01 22:35:10 -0400

    django create superuser

--- runtime.sh
+++ runtime.sh
@@ -5,5 +5,5 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
 cd easy-school
 docker-compose build
-docker-compose run web python manage.py createsuperuser
+docker-compose run web DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --noinput --username=admin --email=admin@example.com
 docker-compose up --detach
